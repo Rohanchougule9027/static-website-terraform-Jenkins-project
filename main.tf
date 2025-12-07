acac475bc30496bea5e0e414d1d3452763e4f824
Rohanchougule9027 <rohanchougule20@gmail.com>
2025-12-07 21:25:34 +0530

    updated

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ provider "aws" {
   
 }
 
-resource "aws_instance" "Staticweb" {
-  ami = var.my_ami
-  instance_type = var.my_instance
-  key_name = var.my_key
-  vpc_security_group_ids = [aws_security_group.staticweb_sg.id]
+resource "aws_instance" "staticweb" {
+  ami = "ami-0933f1385008d33c4"
+  instance_type = "t2.micro"
+  key_name = "jumpserver"
+  vpc_security_group_ids = ["sg-0f7721c15c4568561"]
   user_data = file(var.user_data)
   tags = {
     Name= "staticWeb"
@@ -15,6 +15,6 @@ resource "aws_instance" "Staticweb" {
 }
 
 output "ec2_public_ip" {
-  value = aws_instance.Staticweb.public_ip
+  value = aws_instance.staticweb
 }
 
